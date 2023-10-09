a2728a90ed584d7909a7528d0bd73f6f315fb738
YOUR NAME <YOUR@EMAIL.com>
2023-10-09 23:52:35 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const imageInput = document.getElementById("imageInput");
+    const memeImage = document.getElementById("memeImage");
+    const topText = document.getElementById("topText");
+    const bottomText = document.getElementById("bottomText");
+    const topTextInput = document.getElementById("topTextInput");
+    const bottomTextInput = document.getElementById("bottomTextInput");
+    const generateMeme = document.getElementById("generateMeme");
+    const saveMeme = document.getElementById("saveMeme");
+})
