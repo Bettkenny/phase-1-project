66a41a3d8d0185c527b1b310fd96d5721a451e76
YOUR NAME <YOUR@EMAIL.com>
2023-10-11 16:37:39 +0300

    sugesstion list

--- index.js
+++ index.js
@@ -1,37 +1,23 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const imageInput = document.getElementById("imageInput");
-    const memeImage = document.getElementById("memeImage");
-    const topText = document.getElementById("topText");
-    const bottomText = document.getElementById("bottomText");
-    const topTextInput = document.getElementById("topTextInput");
-    const bottomTextInput = document.getElementById("bottomTextInput");
-    const generateMeme = document.getElementById("generateMeme");
-    const saveMeme = document.getElementById("saveMeme");
-    let memeData ={
-        image: "",
-        topText: "",
-        bottomText: "",
-    };
-    generateMeme.addEventListener("click",function(){
-        memeData.topText =  topTextInput.value;
-        memeData.bottomText=    bottomTextInput.value;
-    });
-    saveMeme.addEventListener("click", function(){
-        fetch("db.json",{
-            method: "POST",
-            headers:{
-                "Content-Type":"application/json",
-            },
-            body:JSON.stringify(memeData),
-        })
-        .then(response => response.json())
-        .then(data =>{
-            console.log("Meme saved:", data);
-            alert("Meme saved successfully!");
-        })
-        .catch(error =>{
-            console.error("Fecth error", error);
-            alert("Error saving meme. Please try again later.")
-        })
-    })
-})
+function suggestFood(){
+    fetch()
+    .then(res => res.json())
+    .then(mov =>mealSugest(mov))
+}
+suggestFoods();
+
+function mealSuggest(foodStuff) {
+  let meals = document.getElementById('foods')
+  
+  for(let dub of foodStuff){
+    let mealList = document.createElement('li')
+    
+    mealList.innerHTML = `<img src="${dub.image}">
+    <h3>${dub.name}</h3>
+    <p>Description: ${dub.description}</p>`;
+    meals.appendChild(mealList);
+  }                 
+
+}
+//const result = document.getElementById("result");
+//const searchBtn = document .getElementById("search-btn");
+//let = ("https://www.themealdb.com/api/json/v1/1/search.php?s=");
