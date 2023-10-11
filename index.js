15e20f26667a4984dd0b7fdc3530c05107106087
YOUR NAME <YOUR@EMAIL.com>
2023-10-11 14:30:16 +0300

    html file

--- index.js
+++ index.js
@@ -7,4 +7,31 @@ document.addEventListener("DOMContentLoaded", function(){
     const bottomTextInput = document.getElementById("bottomTextInput");
     const generateMeme = document.getElementById("generateMeme");
     const saveMeme = document.getElementById("saveMeme");
+    let memeData ={
+        image: "",
+        topText: "",
+        bottomText: "",
+    };
+    generateMeme.addEventListener("click",function(){
+        memeData.topText =  topTextInput.value;
+        memeData.bottomText=    bottomTextInput.value;
+    });
+    saveMeme.addEventListener("click", function(){
+        fetch("db.json",{
+            method: "POST",
+            headers:{
+                "Content-Type":"application/json",
+            },
+            body:JSON.stringify(memeData),
+        })
+        .then(response => response.json())
+        .then(data =>{
+            console.log("Meme saved:", data);
+            alert("Meme saved successfully!");
+        })
+        .catch(error =>{
+            console.error("Fecth error", error);
+            alert("Error saving meme. Please try again later.")
+        })
+    })
 })
